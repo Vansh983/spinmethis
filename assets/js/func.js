a7e5466ea454ee99ff5ceb350dad44202d65b122
Vansh  Sood <Vansh983@3594067.no-reply.drupal.org>
2019-04-23 00:34:08 +0530

    unnamed commit

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
-  $(window).scroll(function(){
-    var k = $(window).scrollTop();
-    console.log(k);
-    if(k>5){
-      for (k < 637; i++) {
-        // $('.landhead').
-      }
-    }
+  // $(window).scroll(function(){
+  //   var k = $(window).scrollTop();
+  //   console.log(k);
+  //   if(k>5){
+  //     for (k < 637; i++) {
+  //       // $('.landhead').
+  //     }
+  //   }
+  // })
+  $('.bars').click(function(){
+    $('.bar1').toggleClass('bar1_active')
+    $('.bar2').toggleClass('bar2_active')
+    $('.bar3').toggleClass('bar3_active')
+    $('.menu').toggleClass('menu-active')
+    $('.bars').toggleClass('bars-active')
+  })
+  $('.btn1').click(function(){
+    $('.mailme').addClass('goto_active');
+
+  })
+  $('.it').click(function(){
+    $('.menu').toggleClass('menu-active')
+    $('.bar1').toggleClass('bar1_active')
+    $('.bar2').toggleClass('bar2_active')
+    $('.bar3').toggleClass('bar3_active')
+    $('.bars').toggleClass('bars-active')
   })
 
 
