34cd453cfbb1560e0f56c75e81e710a448619463
Vansh  Sood <Vansh983@3594067.no-reply.drupal.org>
2019-04-26 02:22:38 +0530

    nav done

--- assets/js/func.js
+++ assets/js/func.js
@@ -8,13 +8,28 @@ $(document).ready(function(){
   //     }
   //   }
   // })
-  $('.bars').click(function(){
-    $('.bar1').toggleClass('bar1_active')
-    $('.bar2').toggleClass('bar2_active')
-    $('.bar3').toggleClass('bar3_active')
-    $('.menu').toggleClass('menu-active')
-    $('.bars').toggleClass('bars-active')
+  $('.bars, .item').click(function(){
+    $('.bar2').toggleClass('bar1-ac')
+    $('.bar1').toggleClass('bar2-ac')
+    $('.bar3').toggleClass('bar3-ac')
+    $('.menu').toggleClass('menu-ac')
+    $('.bars').toggleClass('bars-ac')
+    $('body').toggleClass('flow')
+    $('.tint').toggleClass('tinted')
+    $('.bar-name').toggleClass('bar-name-ac')
   })
+  $('.it1').click(function(){
+			$('body, html').animate({scrollTop:$('.land').offset().top}, 1000)
+	})
+  $('.it2').click(function(){
+			$('body, html').animate({scrollTop:$('.about').offset().top}, 1000)
+	})
+  $('.it3').click(function(){
+			$('body, html').animate({scrollTop:$('.works').offset().top}, 1000)
+	})
+  $('.it4').click(function(){
+			$('body, html').animate({scrollTop:$('.contact').offset().top}, 1000)
+	})
   $('.btn1').click(function(){
     $('.mailme').addClass('goto_active');
 
